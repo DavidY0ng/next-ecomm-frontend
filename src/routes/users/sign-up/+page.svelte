6e5630f48de6f677e7237bb3d517ff6dec244a66
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-14 11:17:10 +0800

    Added sign up page

---
+++ src/routes/users/sign-up/+page.svelte
@@ -0,0 +1,99 @@
+<script>
+    import {PUBLIC_BACKEND_BASE_URL} from '$env/static/public'
+    import { goto } from '$app/navigation';
+
+    let formErrors = {}
+
+    async function createUser(evt) {
+        evt.preventDefault()
+
+        if (evt.target['password'].value != evt.target['confirmPassword'].value) {
+        formErrors['password'] = { message: 'Password confirmation does not match' };
+        return;
+        }
+
+        const userData = {
+            name : evt.target['name'].value,
+            email : evt.target['email'].value,
+            password : evt.target['password'].value,
+        };
+
+        const resp = await fetch(PUBLIC_BACKEND_BASE_URL + '/users', {
+            method : 'POST',
+            mode : 'cors',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (resp.status == 200) {
+            goto ('/');
+        } else {
+            const res = await resp.json();
+            formErrors = res.data;
+        }
+    }
+</script>
+
+<div class = "container mx-auto px-4 py-10">
+    <h1 class="text-center text-xl">Create an Account</h1>
+    <div class="text-center">
+      <a class="link-hover italic text-xs" href="/users/login">Already have an account? Click here to login instead.</a>
+    </div>
+    <div class="flex justify-center items-center mt-8">
+        <form on:submit={createUser} class="w-1/3">
+            <div class="form-control w-full">
+                <label class="label" for="name">
+                    <span class="text">Name</span>
+                </label>
+                <input type="text" name="name" placeholder="johndoe" class="input input-bordered w-full" />
+                {#if 'name' in formErrors}
+                <label class="label" for="name">
+                    <span class="label-text-alt text-red-500">{formErrors['name'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="email">
+                    <span class="text">Email</span>
+                </label>
+                <input type="email" name="email" placeholder="john@example.com" class="input input-bordered w-full" required />
+                {#if 'email' in formErrors}
+                <label class="label" for="email">
+                    <span class="label-text-alt text-red-500">{formErrors['email'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="password">
+                    <span class="text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="" class="input input-bordered w-full" required />
+                {#if 'password' in formErrors}
+                <label class="label" for="password">
+                    <span class="label-text-alt text-red-500">{formErrors['password'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full">
+                <label class="label" for="password">
+                    <span class="text">Confirm Password</span>
+                </label>
+                <input type="password" name="confirmPassword" placeholder="" class="input input-bordered w-full" required />
+                {#if 'password' in formErrors}
+                <label class="label" for="password">
+                    <span class="label-text-alt text-red-500">{formErrors['confirmPassword'].message}</span>
+                </label>
+                {/if}
+            </div>
+    
+            <div class="form-control w-full mt-4">
+                <button class="btn btn-md">Create an Account</button>
+            </div>
+        </form>
+    </div>
+  </div>
