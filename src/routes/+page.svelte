817796d0e0b5b1314cda4c5782c8b7d15b8b7b2b
Yong Chee Ho <yongcheeho@gmail.com>
2023-06-19 15:52:39 +0800

    Added media upload to S3 bucket

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,43 @@
+<svelte:head>
+  <script src="/aws-sdk-s3.min.js"></script>
+</svelte:head>
+
+<script>
+    import { uploadMedia } from '../utils/s3-uploader.js';
+  
+    async function uploadImage(evt) {
+      const [fileName, fileUrl] = await uploadMedia(evt.target['file'].files[0]);
+      // code to make POST request to your backend
+    }
+  </script>
+
 <h1>Welcome to SvelteKit</h1>
 
 <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
 <dialog id="my_modal_1" class="modal">
-  <form method="dialog" class="modal-box">
-    <input type="file" class="file-input file-input-bordered file-input-secondary w-full max-w-xs" />
+<div class = "container">
+    
+</div>
+  <form on:submit|preventDefault={uploadImage} method="dialog" class="modal-box">
+    <input type="file" name="file" class="file-input file-input-bordered file-input-secondary w-full max-w-xs" />
     <div class="form-control w-full max-w-xs">
       <div>
-        <label class="label">
+        <label for ="price" class="label">
           <span class="label-text">Price</span>
         </label>
           <input type="text" placeholder=" " class="input input-bordered w-full max-w-xs" />
-        <label class="label">
+        <label for ="currency" class="label">
           <span class="label-text-alt">USD</span>
         </label>
       </div>
       <div>
-        <label class="label">
+        <label for = "title" class="label">
           <span class="label-text">Title</span>
         </label>
         <input type="text" placeholder=" " class="input input-bordered w-full max-w-xs" />
       </div>
       <div>
-        <label class="label">
+        <label for = "description" class="label">
           <span class="label-text">Description</span>
         </label>
         <textarea class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
